a230979e4395b73f4ebcf8f91ae2eca72e7d9713
Erica Wright <ericaewright@gmail.com>
2015-05-17 00:00:39 +0000

    override 'first person controls' click event

--- money_hoes_clothes/public/js/FirstPersonControls.js
+++ money_hoes_clothes/public/js/FirstPersonControls.js
@@ -87,16 +87,28 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 
 		if ( this.activeLook ) {
 
-			switch ( event.button ) {
+		  mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+		  mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 
-				case 0: this.moveForward = true; break;
-				case 2: this.moveBackward = true; break;
+		  // create a Ray with origin at the mouse position
+		  //   and direction into the scene (camera direction)
+		   var vector = new THREE.Vector3(
+		        ( event.clientX / window.innerWidth ) * 2 - 1,
+		      - ( event.clientY / window.innerHeight ) * 2 + 1,
+		        0.5
+		    );
+		  projector.unprojectVector( vector, camera );
+		  var ray = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
 
-			}
+		  // create an array containing all objects in the scene with which the ray intersects
+		  var intersects = ray.intersectObjects( targetList );
+
+		  if ( intersects.length > 0 ){
+		    ///do something here on click (right now changes colour)
+		    intersects[ 0 ].object.material.color.setHex( Math.random() * 0xffffff );
+		  }
 
 		}
-
-		this.mouseDragOn = true;
 
 	};
 
@@ -107,12 +119,12 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 
 		if ( this.activeLook ) {
 
-			switch ( event.button ) {
+			// switch ( event.button ) {
 
-				case 0: this.moveForward = false; break;
-				case 2: this.moveBackward = false; break;
+			// 	case 0: this.moveForward = false; break;
+			// 	case 2: this.moveBackward = false; break;
 
-			}
+			// }
 
 		}
 
