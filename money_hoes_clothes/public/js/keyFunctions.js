511fab8d6a09fdcae63ec469669e95516fafb6f7
Peter Thicke <peterbthicke@gmail.com>
2015-05-20 01:21:38 +0000

    calculations correct, modifying display

--- money_hoes_clothes/public/js/keyFunctions.js
+++ money_hoes_clothes/public/js/keyFunctions.js
@@ -7,20 +7,29 @@ function goHome() {
 var constellationsOn = false;
 function toggleConstellations() {
   console.log('c clicked');
-  if (constellationsOn == true) {
+  if (constellationsOn) {
     for (i=0; i<totalLines.length; i++) {
       scene.remove(totalLines[i]);
     }
-    constellationsOn = false;
   } else {
     for (i=0; i<totalLines.length; i++) {
       scene.add(totalLines[i]);
-    }
-    constellationsOn = true;
+    }    
   }
+  constellationsOn = !constellationsOn;
 }
+
 
 function speed(number){
   camControls.movementSpeed = number;
 
 }
+
+function findLocation() {
+  var location = Math.sqrt((camera.position.x*camera.position.x)+(camera.position.y*camera.position.y)+(camera.position.z*camera.position.z)) * 3.257;
+  var locationDiv = document.createElement('div');
+  locationDiv.innerHTML = '<h2>Current distance from sun is:</h2>' + location;
+  document.getElementById('output').innerHTML = '';
+  document.getElementById('output').appendChild(locationDiv);
+}
+
