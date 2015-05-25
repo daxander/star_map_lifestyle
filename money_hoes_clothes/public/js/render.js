2e729a7adaac26a6b0bf209e3ca507d2a88c9b4f
David Feimer <davidfeimer@gmail.com>
2015-05-25 20:23:58 +0000

    fix render bug

--- money_hoes_clothes/public/js/render.js
+++ money_hoes_clothes/public/js/render.js
@@ -3,11 +3,17 @@ function render() {
   requestAnimationFrame( render );
   camControls.update(delta);
   findLocation();
+
+
+  renderer.render( scene, camera );
+}
+
+function planetRender() {
+  var delta = clock.getDelta();
+  requestAnimationFrame( render );
+  camControls.update(delta);
+  findLocation();
   animate();
-
-
-
-
 
   renderer.render( scene, camera );
 }
