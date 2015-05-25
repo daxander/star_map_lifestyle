c94ed070aaf909ff0848041e53f1a0b2c5b61ca7
vagrant <vagrant@lighthouse-labs>
2015-05-25 22:37:02 +0000

    ui

---
+++ money_hoes_clothes/public/js/typing.js
@@ -0,0 +1,18 @@
+ function copper() {
+  $(function(){
+          $("#overlay > div").typed({
+              strings: ["Hello<br>^1000 My name is Copernicus. ^1000<br> I'll be your guide to stars."],
+              typeSpeed: 8
+          });
+      });
+}
+// $(function(){
+//     var hitStart = {
+//         strings: ["Salutations Human<br>^1000 My name is Copernicus. ^1000<br> I'll be your guide to stars."],
+//                 ["Let me tell you about some of my features"]
+//         typeSpeed: 8
+//     }
+//     $(".start").click(function(){
+//         $("overlay > div").typed(hitStart);
+//     });
+// });
---
+++ money_hoes_clothes/typingText.js
@@ -0,0 +1,9 @@
+<script>
+  $(function(){
+          $("#overlay > div").typed({
+              strings: ["<h2>Hello</h2>,<br><h1>Welcome to</h1><h1><br><strong>Copernican Perception</strong></h1>"],
+              typeSpeed: 0
+          });
+      });
+</script>
+
