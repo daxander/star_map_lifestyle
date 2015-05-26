c2f3715306d59c32e9464a68f66544cc5115abac
vagrant <vagrant@lighthouse-labs>
2015-05-26 00:26:49 +0000

    hopefully almost finished ui

--- money_hoes_clothes/public/js/typing.js
+++ money_hoes_clothes/public/js/typing.js
@@ -2,11 +2,11 @@
   $(function(){
           $("#overlay > div").typed({
               strings: ["Hello Human.<br> ^1500 My name is <u>Copernicus</u>. ^1500<br> I'll be your guide to stars.", 
-                        "Let me tell you about some of my features. ^1500", "In the top-right is my control panel. ^1500", "In the bottom-left your distance from our Sun. ^1500", "When you click on a star, <br> I will grab some information from the NASA API. ^1500", "If you go too far press the 'H' key to return to the sun. ^1500", "Press the 'X' key to toggle the axis <br> ^1500 and 'C' to toggle constellations", "Happy Stargazing Human!"], 
+                        "Let me tell you about some of my features. ^1500", "In the top-right is my control panel. ^1500", "In the bottom-left your distance from the Sun. ^1500", "When you click on a star, <br> I will grab some information from the NASA API. ^1500", "If you go too far press the 'H' key to return to the sun. ^1500", "Press the 'X' key to toggle the axis <br> ^1500 and 'C' to toggle constellations", "Happy Stargazing Human!"], 
               typeSpeed: 8
           });
       });
-    setTimeout(function(){ overlay(); }, 68000);      
+    setTimeout(function(){ overlay(); }, 75000);      
 }
 // $(function(){
 //     var hitStart = {
